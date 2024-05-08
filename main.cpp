864a31a82dc086e90e3c4159d527532a93dfcdbd
Said Alizada <saidalizada@Saids-MacBook-Pro.local>
2024-05-08 12:59:41 +0300

    Intial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+//LEXER
